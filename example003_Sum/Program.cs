5c08d38dfbd74241ad9c21ee043eb09dfb517027
Pavel <m83819@gmail.com>
2022-08-04 09:02:18 +0700

    example003_Sum_random

--- example003_Sum/Program.cs
+++ example003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int NumbnerA=3;
-int NumberB=5;
+﻿int NumbnerA=new Random().Next(1,10);
+int NumberB=new Random().Next(1,10);;
+Console.WriteLine(NumbnerA);
+Console.WriteLine(NumberB);
 Console.WriteLine(NumbnerA+NumberB);
