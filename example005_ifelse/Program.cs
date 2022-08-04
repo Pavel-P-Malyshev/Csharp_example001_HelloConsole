438c40743657683c5fda4b730302066414569341
Pavel <m83819@gmail.com>
2022-08-04 09:27:01 +0700

    example005_ifelse_lowercase

--- example005_ifelse/Program.cs
+++ example005_ifelse/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("enter user name:");
+string username = Console.ReadLine();
+if(username.ToLower()=="masha"){Console.WriteLine("WOW, that;s Masha");}
+else
+{
+    Console.Write("Hello ");
+    Console.Write(username);
+}
