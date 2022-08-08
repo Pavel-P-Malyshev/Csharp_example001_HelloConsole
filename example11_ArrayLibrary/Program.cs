f567b978c775c0acdd500a67f5e369914308e025
Pavel <m83819@gmail.com>
2022-08-08 12:24:12 +0700

    example11_ArrayLibrary

---
+++ example11_ArrayLibrary/Program.cs
@@ -0,0 +1,29 @@
+﻿void FillArray(int[] collection)
+{
+   int length=collection.Length;
+   int index=0;
+   while(index<length)
+   {
+      collection[index]=new Random().Next(1,10);
+      index++;
+
+   }
+
+}
+
+
+void PrintArray (int[] col)
+{
+
+    int count=col.Length;
+    int position=0;
+    while(position<count) 
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array=new int [10];//new array with size 10 and filled with zeroes;
+FillArray(array);
+PrintArray(array);
