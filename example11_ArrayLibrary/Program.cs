f2fff304a967bb8ec85c0dc79f4fac8f349d3175
Pavel <m83819@gmail.com>
2022-08-08 14:14:37 +0700

    example11_ArrayLibrary1

--- example11_ArrayLibrary/Program.cs
+++ example11_ArrayLibrary/Program.cs
@@ -24,6 +24,28 @@ void PrintArray (int[] col)
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int count=collection.Length;
+    int index=0;
+    int position=-1;
+    while(index<count)
+    {
+        if(collection[index]==find)
+        {
+            position=index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+
 int[] array=new int [10];//new array with size 10 and filled with zeroes;
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+ int pos=IndexOf(array,5);
+Console.WriteLine(pos);
+
